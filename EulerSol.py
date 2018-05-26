5a32b165981314585c2f63b3e06625aa6aa85422
vedantc6 <vedantc6@gmail.com>
2018-05-26 19:43:12 +0530

    Till Q25

--- EulerSol.py
+++ EulerSol.py
@@ -5,6 +5,7 @@ from math import sqrt, ceil
 import fractions
 import time
 import datetime
+import itertools
 
 # Problem 1 : Multiples of 3 and 5 
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
@@ -828,4 +829,97 @@ def names_scores(text):
 
 p += 1
 result, total_time = names_scores("p022_names.txt")
+print ("Solution to Problem {} is {}, solved in {:.5f} seconds".format(p, result, total_time))
+
+# Problem 23: Non-abundant numbers
+# A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+# A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
+# As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
+# Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+
+def divisors(num):
+	div_sum = 0
+	for i in range(1, (num // 2) + 1):
+		if num % i == 0:
+			div_sum += i
+	return div_sum
+
+def abundant_nums(limit):
+    abundant = []
+    for i in range(1, limit):
+        if divisors(i) > i:
+            abundant.append(i)
+
+    return abundant
+
+def non_abundant(limit):
+	start = time.time()
+	abundant = abundant_nums(limit + 1)
+	sums = [0]*(limit+1)
+	non_abundant = 0
+	for x in range(len(abundant)):
+		for y in range(x, len(abundant)):
+			sumOfabundant = abundant[x] + abundant[y]
+			if sumOfabundant <= limit:
+				if sums[sumOfabundant] == 0:
+					sums[sumOfabundant] = sumOfabundant
+
+	for i in range(1, len(sums)):
+		if sums[i] == 0:
+			non_abundant += i
+
+	return non_abundant, time.time() - start
+ 
+p += 1
+result, total_time = non_abundant(28123)
+print ("Solution to Problem {} is {}, solved in {:.5f} seconds".format(p, result, total_time))
+
+# Problem 24: Lexicographic permutations
+# A permutation is an ordered arrangement of objects. For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4. If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+# 012   021   102   120   201   210
+# What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+
+def lexicographic(nums):
+	start = time.time()
+	permutations = list(itertools.permutations(nums))
+	str1 = ''.join(str(x) for x in permutations[999999])
+	return str1, time.time() - start
+
+p += 1
+result, total_time = lexicographic([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+print ("Solution to Problem {} is {}, solved in {:.5f} seconds".format(p, result, total_time))
+
+# Problem 25: 1000-digit fibonacci number
+# The Fibonacci sequence is defined by the recurrence relation:
+# Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+# Hence the first 12 terms will be:
+# F1 = 1
+# F2 = 1
+# F3 = 2
+# F4 = 3
+# F5 = 5
+# F6 = 8
+# F7 = 13
+# F8 = 21
+# F9 = 34
+# F10 = 55
+# F11 = 89
+# F12 = 144
+# The 12th term, F12, is the first term to contain three digits.
+# What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+
+def fibonacci():
+	start = time.time()
+	series = [0, 1, 1]
+	start = 3
+	end = 5000
+	for i in range(start, end):
+		series.append(series[i-1] + series[i-2])
+		if len(str(series[i])) >= 1000:
+			return i, time.time() - start
+		else:
+			continue
+
+p += 1
+result, total_time = fibonacci()
 print ("Solution to Problem {} is {}, solved in {:.5f} seconds".format(p, result, total_time))
