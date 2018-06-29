087d014e6ec30ab4c6e5ed40a1699b6d2c0f8af4
Vedant Choudhary <vedantc6@gmail.com>
2018-06-29 23:58:54 +0530

    Q28

--- EulerSol.py
+++ EulerSol.py
@@ -1017,3 +1017,26 @@ p += 1
 result, total_time = quad_primes(1000)
 print ("Solution to Problem {} is {}, solved in {:.5f} seconds".format(p, result, total_time))
 
+# Problem 28: Number spiral diagonals
+# Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
+# 21 22 23 24 25
+# 20  7  8  9 10
+# 19  6  1  2 11
+# 18  5  4  3 12
+# 17 16 15 14 13
+# It can be verified that the sum of the numbers on the diagonals is 101.
+# What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
+
+def num_spiral(n):
+	if n == 1:
+		return 1
+	return 4*n*n - 6*n - 6 + num_spiral(n-2)
+
+def num_spiral_wrapper(n):
+	start = time.time()
+	value = num_spiral(n)
+	return value, time.time() - start
+	
+p += 1
+result, total_time = num_spiral_wrapper(1001)
+print ("Solution to Problem {} is {}, solved in {:.5f} seconds".format(p, result, total_time))
